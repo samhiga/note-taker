09637e8bb9993b09b6b82acee9d45e33a30230b6
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-27 19:13:38 -0700

    fixed issue for saved notes not displaying on page

--- server.js
+++ server.js
@@ -4,45 +4,46 @@ const {readNotes, writeNotes} = require('./utils/dbHelpers')
 const PORT = 3001;
 
 const app = express()
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
- 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
-  
-
-app.post('/api/notes', (req, res) => {
-
+app.get('/api/notes', (req, res) => {
     readNotes()
-        .then(notes => {
-            const parsedNotes = JSON.parse(notes)
-            const newNote = {
-                title: req.body.title,
-                text: req.body.text
+    .then(notes => {
+      res.json(JSON.parse(notes));
+    })
+});
+ 
+app.post('/api/notes', (req, res) => {
+      
+      readNotes()
+      .then(notes => {
+          const parsedNotes = JSON.parse(notes)
+          const newNote = {
+              title: req.body.title,
+              text: req.body.text
             }
             parsedNotes.push(newNote)
             writeNotes(parsedNotes)
-                .then(() => {
-                    res.json(parsedNotes)
-                })
+            .then(() => {
+                res.json(parsedNotes)
+            })
         })
         .catch(err => {
-        console.error(err)
+            console.error(err)
+        })
+         
     })
-
-
-
-})
-
-app.listen(PORT, () =>
+    
+    app.get('/notes', (req, res) =>
+      res.sendFile(path.join(__dirname, '/public/notes.html'))
+    );
+    app.get('/', (req, res) =>
+      res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+    
+    app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
